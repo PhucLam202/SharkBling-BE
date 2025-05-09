1e5b523218ed2f20e0a81563084091009e56e112
phuclam <lamthanhphucit@gmail.com>
2025-05-09 15:49:02 +0700

    hotfix: error with cors

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,10 +4,11 @@ export default defineConfig({
   entry: ['index.ts'],
   format: ['esm'],
   platform: 'node',
-  target: 'node18',
+  target: 'node20',
   banner: {
     js: "import { createRequire } from 'module'; const require = createRequire(import.meta.url);"
   },
   noExternal: [/.*/],
   clean: true,
+  dts:false
 });
